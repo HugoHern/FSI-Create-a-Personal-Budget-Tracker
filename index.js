5510156d04ac68f44ccee7a17a921fd123359c04
HugoHern <hugo8954@gmail.com>
2022-01-25 19:05:38 -0500

    Adding my changes to the project

--- index.js
+++ index.js
@@ -3,3 +3,35 @@ let monthlyExpenses = 0
 let annualExpenses = 0
 
 // Your Code Here
+
+for(let i = 0; i < weeklyExpenseQuestions.length; i++)
+{
+let stringAnswer = window.prompt(weeklyExpenseQuestions[i])
+let numberAnswer = parseFloat(stringAnswer)
+weeklyExpenses = weeklyExpenses + numberAnswer
+
+console.log(weeklyExpenses)
+}
+
+for(let i = 0; i < monthlyExpenseQuestions.length; i++)
+{
+    let stringAnswer = window.prompt(monthlyExpenseQuestions[i])
+    let numberAnswer = parseFloat(stringAnswer)
+    monthlyExpenses = monthlyExpenses + numberAnswer
+
+    console.log(monthlyExpenses)
+
+}
+
+for(let i = 0; i < annualExpenseQuestions; i++)
+{
+    let stringAnswer = window.prompt(annualExpenseQuestions[i])
+    let numberAnswer = parseFloat(stringAnswer)
+    annualExpenses = annualExpenses + numberAnswer
+
+    console.log(annualExpenses)
+
+}
+
+
+
